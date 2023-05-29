0a6bed940fc18f7a703dfe60f07924d2b656132b
edefries <132692603+edefries@users.noreply.github.com>
2023-05-29 17:18:45 +0100

    Update code_updated_loop.py

--- code_updated_loop.py
+++ code_updated_loop.py
@@ -1,6 +1,5 @@
 import sys, random, time
 import horoscopes #separate file the programme uses to import the horoscopes without them taking up a load of space in this main code
-# currently "import horoscopes" doesn't work
 
 def slowtype(str):
     for letter in str:
@@ -33,7 +32,7 @@ time.sleep(0.85)
 
 # listofmonths = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"] #so the programme knows what months are actual dates
 # ^^ didn't want to delete this incase the dictionary failed
-horoscopesstarsign = ["Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn"] #so the programme knows what horoscopes.starsigns are
+starsign = ["Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn"] #so the programme knows what starsigns are
 
 
 
@@ -87,7 +86,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are an Aquarius")
         elif month == "february" and 1 <= date <= 18:
             slowtype("%s \n" % "You are an Aquarius")
-        ss = horoscopesstarsign[0] #makes the programme know which starsign it has just output
+        ss = starsign[0] #makes the programme know which starsign it has just output
         break
         
 # for loop basically tests for membership in the list that the users info was appended to
@@ -99,7 +98,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are a Pisces")
         elif month == "march" and 1 <= date <= 20:
             slowtype("%s \n" % "You are a Pisces")
-        ss = horoscopesstarsign[1]
+        ss = starsign[1]
         break
 
 for item in inputmonth:
@@ -108,7 +107,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are an Aries")
         elif month == "april" and 1 <= date <= 19:
             slowtype("%s \n" % "You are an Aries")
-        ss = horoscopesstarsign[2]
+        ss = starsign[2]
         break
 
 for item in inputmonth:
@@ -117,7 +116,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are a Taurus")
         elif month == "may" and 1 <= date <= 20:
             slowtype("%s \n" % "You are a Taurus")
-        ss = horoscopesstarsign[3]
+        ss = starsign[3]
         break
 
 for item in inputmonth:
@@ -126,7 +125,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are a Gemini")
         elif month == "june" and 1 <= date <= 20:
             slowtype("%s \n" % "You are a Gemini")
-        ss = horoscopesstarsign[4]
+        ss = starsign[4]
         break
     
 for item in inputmonth:
@@ -135,7 +134,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are a Cancer")
         elif month == "july" and 1 <= date <= 22:
             slowtype("%s \n" % "You are a Cancer")
-        ss = horoscopesstarsign[5]
+        ss = starsign[5]
         break
 
 for item in inputmonth:
@@ -144,7 +143,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are a Leo")
         elif month == "august" and 1 <= date <= 22:
             slowtype("%s \n" % "You are a Leo")
-        ss = horoscopesstarsign[6]
+        ss = starsign[6]
         break
 
 for item in inputmonth:
@@ -153,7 +152,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are a Virgo")
         elif month == "september" and 1 <= date <= 22:
             slowtype("%s \n" % "You are a Virgo")
-        ss = horoscopesstarsign[7]
+        ss = starsign[7]
         break
 
 for item in inputmonth:
@@ -162,7 +161,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are a Libra")
         elif month == "october" and 1 <= date <= 22:
             slowtype("%s \n" % "You are a Libra")
-        ss = horoscopesstarsign[8]
+        ss = starsign[8]
         break
     
 for item in inputmonth:
@@ -171,7 +170,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are a Scorpio")
         elif month == "november" and 1 <= date <= 21:
             slowtype("%s \n" % "You are a Scorpio")
-        ss = horoscopesstarsign[9]
+        ss = starsign[9]
         break
 
 for item in inputmonth:
@@ -180,7 +179,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are a Sagittarius")
         elif month == "december" and 1 <= date <= 22:
             slowtype("%s \n" % "You are a Sagittarius")
-        ss = horoscopesstarsign[10]
+        ss = starsign[10]
         break
 
 for item in inputmonth:
@@ -189,7 +188,7 @@ for item in inputmonth:
             slowtype("%s \n" % "You are a Capricorn")
         elif month == "january" and 1 <= date <= 19:
             slowtype("%s \n" % "You are a Capricorn")
-        ss = horoscopesstarsign[11]
+        ss = starsign[11]
         break
 
 time.sleep(0.85)
@@ -202,160 +201,160 @@ horoscope = input().lower()
 future = ["love", "health", "family", "work", "finance", "travel"] #so the programme knows which horoscopes can be requested
 
 
-if horoscope == "love" and ss == horoscopes.starsign[0]:
+if horoscope == "love" and ss == starsign[0]:
     slowtype(random.choice(horoscopes.lovelist0)) #randomly picks from the list to output the horoscope
-elif horoscope == "health" and ss == horoscopes.starsign[0]:
+elif horoscope == "health" and ss == starsign[0]:
     slowtype(random.choice(horoscopes.healthlist0))
-elif horoscope == "family" and ss == horoscopes.starsign[0]:
+elif horoscope == "family" and ss == starsign[0]:
     slowtype(random.choice(horoscopes.familylist0))
-elif horoscope == "work" and ss == horoscopes.starsign[0]:
+elif horoscope == "work" and ss == starsign[0]:
     slowtype(random.choice(horoscopes.worklist0))
-elif horoscope == "finance" and ss == horoscopes.starsign[0]:
+elif horoscope == "finance" and ss == starsign[0]:
     slowtype(random.choice(horoscopes.financelist0))
-elif horoscope == "travel" and ss == horoscopes.starsign[0]:
+elif horoscope == "travel" and ss == starsign[0]:
     slowtype(random.choice(horoscopes.travellist0))
       
-elif horoscope == "love" and ss == horoscopes.starsign[1]:
+elif horoscope == "love" and ss == starsign[1]:
     slowtype(random.choice(horoscopes.lovelist1))
-elif horoscope == "health" and ss == horoscopes.starsign[1]:
+elif horoscope == "health" and ss == starsign[1]:
     slowtype(random.choice(horoscopes.healthlist1))
-elif horoscope == "family" and ss == horoscopes.starsign[1]:
+elif horoscope == "family" and ss == starsign[1]:
     slowtype(random.choice(horoscopes.familylist1))
-elif horoscope == "work" and ss == horoscopes.starsign[1]:
+elif horoscope == "work" and ss == starsign[1]:
     slowtype(random.choice(horoscopes.worklist1))
-elif horoscope == "finance" and ss == horoscopes.starsign[1]:
+elif horoscope == "finance" and ss == starsign[1]:
     slowtype(random.choice(horoscopes.financelist1))
-elif horoscope == "travel" and ss == horoscopes.starsign[1]:
+elif horoscope == "travel" and ss == starsign[1]:
     slowtype(random.choice(horoscopes.travellist1))
 
-elif horoscope == "love" and ss == horoscopes.starsign[2]:
+elif horoscope == "love" and ss == starsign[2]:
     slowtype(random.choice(horoscopes.lovelist2))
-elif horoscope == "health" and ss == horoscopes.starsign[2]:
+elif horoscope == "health" and ss == starsign[2]:
     slowtype(random.choice(horoscopes.healthlist2))
-elif horoscope == "family" and ss == horoscopes.starsign[2]:
+elif horoscope == "family" and ss == starsign[2]:
     slowtype(random.choice(horoscopes.familylist2))
-elif horoscope == "work" and ss == horoscopes.starsign[2]:
+elif horoscope == "work" and ss == starsign[2]:
     slowtype(random.choice(horoscopes.worklist2))
-elif horoscope == "finance" and ss == horoscopes.starsign[2]:
+elif horoscope == "finance" and ss == starsign[2]:
     slowtype(random.choice(horoscopes.financelist2))
-elif horoscope == "travel" and ss == horoscopes.starsign[2]:
+elif horoscope == "travel" and ss == starsign[2]:
     slowtype(random.choice(horoscopes.travellist2))
  
-elif horoscope == "love" and ss == horoscopes.starsign[3]:
+elif horoscope == "love" and ss == starsign[3]:
     slowtype(random.choice(horoscopes.lovelist3))
-elif horoscope == "health" and ss == horoscopes.starsign[3]:
+elif horoscope == "health" and ss == starsign[3]:
     slowtype(random.choice(horoscopes.healthlist3))
-elif horoscope == "family" and ss == horoscopes.starsign[3]:
+elif horoscope == "family" and ss == starsign[3]:
     slowtype(random.choice(horoscopes.familylist3))
-elif horoscope == "work" and ss == horoscopes.starsign[3]:
+elif horoscope == "work" and ss == starsign[3]:
     slowtype(random.choice(horoscopes.worklist3))
-elif horoscope == "finance" and ss == horoscopes.starsign[3]:
+elif horoscope == "finance" and ss == starsign[3]:
     slowtype(random.choice(horoscopes.financelist3))
-elif horoscope == "travel" and ss == horoscopes.starsign[3]:
+elif horoscope == "travel" and ss == starsign[3]:
     slowtype(random.choice(horoscopes.travellist3))
 
-elif horoscope == "love" and ss == horoscopes.starsign[4]:
+elif horoscope == "love" and ss == starsign[4]:
     slowtype(random.choice(horoscopes.lovelist4))
-elif horoscope == "health" and ss == horoscopes.starsign[4]:
+elif horoscope == "health" and ss == starsign[4]:
     slowtype(random.choice(horoscopes.healthlist4))
-elif horoscope == "family" and ss == horoscopes.starsign[4]:
+elif horoscope == "family" and ss == starsign[4]:
     slowtype(random.choice(horoscopes.familylist4))
-elif horoscope == "work" and ss == horoscopes.starsign[4]:
+elif horoscope == "work" and ss == starsign[4]:
     slowtype(random.choice(horoscopes.worklist4))
-elif horoscope == "finance" and ss == horoscopes.starsign[4]:
+elif horoscope == "finance" and ss == starsign[4]:
     slowtype(random.choice(horoscopes.financelist4))
-elif horoscope == "travel" and ss == horoscopes.starsign[4]:
+elif horoscope == "travel" and ss == starsign[4]:
     slowtype(random.choice(horoscopes.travellist4))
     
-elif horoscope == "love" and ss == horoscopes.starsign[5]:
+elif horoscope == "love" and ss == starsign[5]:
     slowtype(random.choice(horoscopes.lovelist5))
-elif horoscope == "health" and ss == horoscopes.starsign[5]:
+elif horoscope == "health" and ss == starsign[5]:
     slowtype(random.choice(horoscopes.healthlist5))
-elif horoscope == "family" and ss == horoscopes.starsign[5]:
+elif horoscope == "family" and ss == starsign[5]:
     slowtype(random.choice(horoscopes.familylist5))
-elif horoscope == "work" and ss == horoscopes.starsign[5]:
+elif horoscope == "work" and ss == starsign[5]:
     slowtype(random.choice(horoscopes.worklist5))
-elif horoscope == "finance" and ss == horoscopes.starsign[5]:
+elif horoscope == "finance" and ss == starsign[5]:
     slowtype(random.choice(horoscopes.financelist5))
-elif horoscope == "travel" and ss == horoscopes.starsign[5]:
+elif horoscope == "travel" and ss == starsign[5]:
     slowtype(random.choice(horoscopes.travellist5))
    
-elif horoscope == "love" and ss == horoscopes.starsign[6]:
+elif horoscope == "love" and ss == starsign[6]:
     slowtype(random.choice(horoscopes.lovelist6))
-elif horoscope == "health" and ss == horoscopes.starsign[6]:
+elif horoscope == "health" and ss == starsign[6]:
     slowtype(random.choice(horoscopes.healthlist6))
-elif horoscope == "family" and ss == horoscopes.starsign[6]:
+elif horoscope == "family" and ss == starsign[6]:
     slowtype(random.choice(horoscopes.familylist6))
-elif horoscope == "work" and ss == horoscopes.starsign[6]:
+elif horoscope == "work" and ss == starsign[6]:
     slowtype(random.choice(horoscopes.worklist6))
-elif horoscope == "finance" and ss == horoscopes.starsign[6]:
+elif horoscope == "finance" and ss == starsign[6]:
     slowtype(random.choice(horoscopes.financelist6))
-elif horoscope == "travel" and ss == horoscopes.starsign[6]:
+elif horoscope == "travel" and ss == starsign[6]:
     slowtype(random.choice(horoscopes.travellist6))
    
-elif horoscope == "love" and ss == horoscopes.starsign[7]:
+elif horoscope == "love" and ss == starsign[7]:
     slowtype(random.choice(horoscopes.lovelist7))
-elif horoscope == "health" and ss == horoscopes.starsign[7]:
+elif horoscope == "health" and ss == starsign[7]:
     slowtype(random.choice(horoscopes.healthlist7))
-elif horoscope == "family" and ss == horoscopes.starsign[7]:
+elif horoscope == "family" and ss == starsign[7]:
     slowtype(random.choice(horoscopes.familylist7))
-elif horoscope == "work" and ss == horoscopes.starsign[7]:
+elif horoscope == "work" and ss == starsign[7]:
     slowtype(random.choice(horoscopes.worklist7))
-elif horoscope == "finance" and ss == horoscopes.starsign[7]:
+elif horoscope == "finance" and ss == starsign[7]:
     slowtype(random.choice(horoscopes.financelist7))
-elif horoscope == "travel" and ss == horoscopes.starsign[7]:
+elif horoscope == "travel" and ss == starsign[7]:
     slowtype(random.choice(horoscopes.travellist7))
     
-elif horoscope == "love" and ss == horoscopes.starsign[8]:
+elif horoscope == "love" and ss == starsign[8]:
     slowtype(random.choice(horoscopes.lovelist8))
-elif horoscope == "health" and ss == horoscopes.starsign[8]:
+elif horoscope == "health" and ss == starsign[8]:
     slowtype(random.choice(horoscopes.healthlist8))
-elif horoscope == "family" and ss == horoscopes.starsign[8]:
+elif horoscope == "family" and ss == starsign[8]:
     slowtype(random.choice(horoscopes.familylist8))
-elif horoscope == "work" and ss == horoscopes.starsign[8]:
+elif horoscope == "work" and ss == starsign[8]:
     slowtype(random.choice(horoscopes.worklist8))
-elif horoscope == "finance" and ss == horoscopes.starsign[8]:
+elif horoscope == "finance" and ss == starsign[8]:
     slowtype(random.choice(horoscopes.financelist8))
-elif horoscope == "travel" and ss == horoscopes.starsign[8]:
+elif horoscope == "travel" and ss == starsign[8]:
     slowtype(random.choice(horoscopes.travellist8))
      
-elif horoscope == "love" and ss == horoscopes.starsign[9]:
+elif horoscope == "love" and ss == starsign[9]:
     slowtype(random.choice(horoscopes.lovelist9))
-elif horoscope == "health" and ss == horoscopes.starsign[9]:
+elif horoscope == "health" and ss == starsign[9]:
     slowtype(random.choice(horoscopes.healthlist9))
-elif horoscope == "family" and ss == horoscopes.starsign[9]:
+elif horoscope == "family" and ss == starsign[9]:
     slowtype(random.choice(horoscopes.familylist9))
-elif horoscope == "work" and ss == horoscopes.starsign[9]:
+elif horoscope == "work" and ss == starsign[9]:
     slowtype(random.choice(horoscopes.worklist9))
-elif horoscope == "finance" and ss == horoscopes.starsign[9]:
+elif horoscope == "finance" and ss == starsign[9]:
     slowtype(random.choice(horoscopes.financelist9))
-elif horoscope == "travel" and ss == horoscopes.starsign[9]:
+elif horoscope == "travel" and ss == starsign[9]:
     slowtype(random.choice(horoscopes.travellist9))
     
-elif horoscope == "love" and ss == horoscopes.starsign[10]:
+elif horoscope == "love" and ss == starsign[10]:
     slowtype(random.choice(horoscopes.lovelist10))
-elif horoscope == "health" and ss == horoscopes.starsign[10]:
+elif horoscope == "health" and ss == starsign[10]:
     slowtype(random.choice(horoscopes.healthlist10))
-elif horoscope == "family" and ss == horoscopes.starsign[10]:
+elif horoscope == "family" and ss == starsign[10]:
     slowtype(random.choice(horoscopes.familylist10))
-elif horoscope == "work" and ss == horoscopes.starsign[10]:
+elif horoscope == "work" and ss == starsign[10]:
     slowtype(random.choice(horoscopes.worklist10))
-elif horoscope == "finance" and ss == horoscopes.starsign[10]:
+elif horoscope == "finance" and ss == starsign[10]:
     slowtype(random.choice(horoscopes.financelist10))
-elif horoscope == "travel" and ss == horoscopes.starsign[10]:
+elif horoscope == "travel" and ss == starsign[10]:
     slowtype(random.choice(horoscopes.travellist10))
     
-elif horoscope == "love" and ss == horoscopes.starsign[11]:
+elif horoscope == "love" and ss == starsign[11]:
     slowtype(random.choice(horoscopes.lovelist11))
-elif horoscope == "health" and ss == horoscopes.starsign[11]:
+elif horoscope == "health" and ss == starsign[11]:
     slowtype(random.choice(horoscopes.healthlist11))
-elif horoscope == "family" and ss == horoscopes.starsign[11]:
+elif horoscope == "family" and ss == starsign[11]:
     slowtype(random.choice(horoscopes.familylist11))
-elif horoscope == "work" and ss == horoscopes.starsign[11]:
+elif horoscope == "work" and ss == starsign[11]:
     slowtype(random.choice(horoscopes.worklist11))
-elif horoscope == "finance" and ss == horoscopes.starsign[11]:
+elif horoscope == "finance" and ss == starsign[11]:
     slowtype(random.choice(horoscopes.financelist11))
-elif horoscope == "travel" and ss == horoscopes.starsign[11]:
+elif horoscope == "travel" and ss == starsign[11]:
     slowtype(random.choice(horoscopes.travellist11))
 
 if horoscope not in future:
